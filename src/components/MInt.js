ac4147d66e4884c617fe0b899f1ec5f522ae609b
Vanchowe22 <79274004+Vanchowe22@users.noreply.github.com>
2022-04-29 01:31:18 +0300

    fixing styles

--- src/components/MInt.js
+++ src/components/MInt.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useMoralis, useWeb3Contract } from "react-moralis";
+import { Link } from "react-router-dom";
 import { abi, contractAddress } from '../constants/contract';
 import IsAuth from "../hoc/IsAuth";
 const Mint = () => {
@@ -12,7 +13,6 @@ const Mint = () => {
         }
         runContractFunction();
     };
-
 
     const { runContractFunction } = useWeb3Contract({
         abi: abi,
@@ -28,10 +28,10 @@ const Mint = () => {
         <section className="mint-wrapper" >
             <div className="description" >
                 <h2>WeirdNeckz consists of 10,000 Weird collectibles</h2>
-                <p className="info" > The initial collection will not only include cool art, but also work as a membership allowing access to future projects incl.game development, 3d art, experiences and more…</p >
+                <p className="info" > The initial collection will not only include cool art, but also work as a membership allowing access to future projects incl. game development, 3d art, experiences and more…</p >
 
-                <div className="actions-wrapper" >
-                    <button className="btn btn-green" onClick={() => mint()}>Mint</button>
+                <div className="actions-wrapperr" >
+                    <Link to={"/mint"} className="btn btn-green" onClick={() => mint()}>Mint</Link>
                     <div className="input-wrapper" >
                         <img src="/images/minus.png" onClick={() => setAmount(x => x === 0 ? x = 0 : x - 1)} />
                         <input value={amount} type="number" className="input" onChange={(x) => setAmount(x.target.value)} />
